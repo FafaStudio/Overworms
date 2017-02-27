eb4b8e9a9715dd4376329e9e5e59ffd8e3ded8d2
DESKTOP-5FDJABE\sulex <sulexa95@gmail.com>
2017-02-27 17:18:11 +0100

    Test surrement buggé

---
+++ Overworms/Assets/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Q))//left
+        {
+            this.transform.Translate(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))//right
+        {
+            this.transform.Translate(1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))//jump
+        {
+            this.transform.Translate(0, 1, 0);
+        }
+    }
+
+
+}
