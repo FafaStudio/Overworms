7c71f42fb7457cc54a1a1a7c9788fda454d01d84
Bombix34 <bombix34@gmail.com>
2017-03-23 16:50:57 +0100

    - Ajout des stats du personnage : accesseurs, setter, variables courantes … - Debut des mouvements du personnage : mouvement + jump

---
+++ Overworms/Assets/Scripts/Heroes/HeroManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroManager : MonoBehaviour {
+
+	//variables d'initialisations pour rendre public les variables statics
+	public string initNOM;
+	public int initPV, initATTACK, initDEFENSE, initINIT, initFIELDOFVIEW, initMOVESPEED, initENDURANCE;
+	//____________________________________________________________________
+
+	static string NOM;
+
+	static int PV;
+	int curPv;
+
+	static int ATTACK;
+	int curAttack;
+
+	static int DEFENSE;
+	int curDefense;
+
+	static int INIT;
+	int curInit;
+
+	static int FIELD_OF_VIEW;
+	int curFieldOfView;
+
+	static int MOVE_SPEED;
+	int curMoveSpeed;
+
+	static int ENDURANCE;
+	int curEndurance;
+
+	bool myTurn = false;
+
+	void initStats(){
+	//initiation des variables static et courantes au start() 
+		NOM = initNOM;
+		PV = curPv = initPV;
+		ATTACK = curAttack = initATTACK;
+		DEFENSE = curDefense = initDEFENSE;
+		INIT = curInit = initINIT;
+		FIELD_OF_VIEW = curFieldOfView = initFIELDOFVIEW;
+		MOVE_SPEED = curMoveSpeed = initMOVESPEED;
+		ENDURANCE = curEndurance = initENDURANCE;
+	}
+
+	void Start () {
+		initStats ();
+	}
+
+	void Die(){
+		gameObject.SetActive (false);
+	}
+
+	//SET-GET TOUR DE JEU
+	public bool getIsMyTurn(){return myTurn;}
+	public void setIsMyTurn(bool val){myTurn = val;}
+
+	//ACCESSEURS VARIABLES STATIQUES
+	public string getNOM(){return NOM;}
+	public int getPV(){return PV;}
+	public int getATTACK(){return ATTACK;}
+	public int getDEFENSE(){return DEFENSE;}
+	public int getINIT(){return INIT;}
+	public int getFIELDOFVIEW(){return FIELD_OF_VIEW;}
+	public int getMOVESPEED(){return MOVE_SPEED;}
+	public int getENDURANCE(){return ENDURANCE;}
+
+	//ACCESSEURS VARIABLES COURANTES
+	public int getCurPV(){return curPv;}
+	public int getCurAttack(){return curAttack;}
+	public int getCurDefense(){return curDefense;}
+	public int getCurInit(){return curInit;}
+	public int getCurFieldOfView(){return curFieldOfView;}
+	public int getCurMoveSpeed(){return curMoveSpeed;}
+	public int getCurEndurance(){return curEndurance;}
+
+	//MODIFCATEURS DES STATS
+	public void gainLoseAttack(int val){curAttack = ATTACK + val;}
+	public void gainLoseDefense(int val){curDefense = DEFENSE + val;}
+	public void gainLoseInit(int val){curInit = INIT + val;}
+	public void gainLoseFieldOfView(int val){curFieldOfView = FIELD_OF_VIEW + val;}
+	public void gainLoseMoveSpeed(int val){curMoveSpeed = MOVE_SPEED + val;}
+	public void gainLoseEndurance(int val){curEndurance = ENDURANCE + val;}
+	public void gainLosePv(int val){
+		curPv += val;
+		if (curPv <= 0) 
+			Die ();
+	}
+}
---
+++ Overworms/Assets/Scripts/Heroes/MovementController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+    
+	HeroManager hero;
+	float heroSpeed;
+	Rigidbody2D body;
+
+	bool groundTouch;
+	bool canJump;
+	float timerSaut;
+	public float MaxSautTime;
+
+
+	void Start () {
+		hero = GetComponent<HeroManager> ();
+		body = GetComponent<Rigidbody2D> ();
+		timerSaut = MaxSautTime;
+		canJump=true;
+	}
+
+	void Update () {
+		heroSpeed = hero.getCurMoveSpeed ();
+		checkJump ();
+		Jump ();
+	}
+
+	void FixedUpdate(){
+		movement ();
+	}
+
+	private void checkJump(){
+		if (!canJump) {
+			timerSaut -= Time.deltaTime;
+		}
+		if (timerSaut <= 0f) {
+			canJump = true;
+			timerSaut = MaxSautTime;
+		}
+	}
+
+	private void Jump(){
+		if (groundTouch && canJump && (timerSaut <= 0 || timerSaut == MaxSautTime) && (Input.GetKey (KeyCode.Space))) {
+			body.AddForce(new Vector2(0f, heroSpeed ), ForceMode2D.Impulse);
+			canJump = false;
+			groundTouch = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Ground"){
+			groundTouch = true;
+		}
+	}
+
+	private void movement(){
+		if (Input.GetKey (KeyCode.Q)) {
+			body.velocity = new Vector2 (-heroSpeed*0.05f, body.velocity.y);
+		} else if (Input.GetKey (KeyCode.D)) {
+			body.velocity = new Vector2 (heroSpeed*0.05f, body.velocity.y);
+
+		}
+		else 
+		{
+			body.velocity = new Vector2 (0f, body.velocity.y);
+		}
+	}
+
+
+
+}
